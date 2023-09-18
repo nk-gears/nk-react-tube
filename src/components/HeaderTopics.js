c4536dfefdf7bc7c14ac10ce869c0fd930f425fe
NirmalK <nirmalk.web@gmail.com>
2023-09-18 09:52:05 +0530

    cleanup

---
+++ src/components/HeaderTopics.js
@@ -0,0 +1,114 @@
+import React, { Component, Fragment } from "react";
+import Main from "./Main";
+//import axios from 'axios';
+
+class HeaderTopics extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickedVal: null,
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler = (event) => {
+    this.setState({
+      clickedVal: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <div className="row HeaderTopics">
+          <div className="col-12">
+            <button
+              className="HTbuttons"
+              value=""
+              type="Submit"
+              onClick={this.clickHandler}
+            >
+              {" "}
+              All{" "}
+            </button>{" "}
+            <button
+              className="HTbuttons"
+              value="JSX"
+              type="Submit"
+              onClick={this.clickHandler}
+            >
+              {" "}
+              JSX{" "}
+            </button>{" "}
+            <button
+              className="HTbuttons"
+              value="Componenets"
+              type="Submit"
+              onClick={this.clickHandler}
+            >
+              {" "}
+              Components{" "}
+            </button>{" "}
+            <button
+              className="HTbuttons"
+              value="Props"
+              type="Submit"
+              onClick={this.clickHandler}
+            >
+              {" "}
+              Props{" "}
+            </button>{" "}
+            <button
+              className="HTbuttons"
+              value="State"
+              type="Submit"
+              onClick={this.clickHandler}
+            >
+              {" "}
+              State{" "}
+            </button>{" "}
+            <button
+              className="HTbuttons"
+              value="Lifecycle%20Methods"
+              type="Submit"
+              onClick={this.clickHandler}
+            >
+              {" "}
+              Lifecycle Methods{" "}
+            </button>{" "}
+            <button
+              className="HTbuttons"
+              value="Event%Handling"
+              type="Submit"
+              onClick={this.clickHandler}
+            >
+              {" "}
+              Event Handling{" "}
+            </button>{" "}
+            <button
+              className="HTbuttons"
+              value="Hooks"
+              type="Submit"
+              onClick={this.clickHandler}
+            >
+              {" "}
+              Hooks{" "}
+            </button>{" "}
+            <button
+              className="HTbuttons"
+              value="API"
+              type="Submit"
+              onClick={this.clickHandler}
+            >
+              {" "}
+              API{" "}
+            </button>{" "}
+          </div>{" "}
+        </div>{" "}
+        <Main urlSearch={this.state.clickedVal} />{" "}
+      </Fragment>
+    );
+  }
+}
+
+export default HeaderTopics;
---
+++ src/components/SideNavItems.js
@@ -0,0 +1,29 @@
+let SideNavItems = (props) => {
+  let playlistTitle = props.playlist.snippet.title.replace(/&amp;/g, "&");
+
+  if (playlistTitle.length > 33) {
+    playlistTitle = playlistTitle.substring(0, 30) + "...";
+  }
+
+  return (
+    <a
+      className="sideNavLink"
+      href={
+        "https://www.youtube.com/playlist?list=" + props.playlist.id.playlistId
+      }
+    >
+      <div className="sideNavItem">
+        <div className="playlistLink"> {playlistTitle} </div>{" "}
+        <div>
+          {" "}
+          <span className="playlistChannel">
+            {" "}
+            {props.playlist.snippet.channelTitle}{" "}
+          </span>
+        </div>
+      </div>{" "}
+    </a>
+  );
+};
+
+export default SideNavItems;
